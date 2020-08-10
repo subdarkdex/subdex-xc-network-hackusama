6d75dd57e7085d9d47fda16c1e811e0c2aa7dc1b
bwty <whalelephant@users.noreply.github.com>
2020-08-10 09:51:34 +0700

    app js

--- app/index.js
+++ app/index.js
@@ -1,46 +1,34 @@
-const { ApiPromise, WsProvider } = require('@polkadot/api');
-const { Keyring } = require('@polkadot/keyring');
-
-
-const wsProvider = new WsProvider('ws://127.0.0.1:7744');
-
+const { ApiPromise, WsProvider } = require("@polkadot/api");
+const { Keyring } = require("@polkadot/keyring");
+const wsProvider = new WsProvider("ws://127.0.0.1:7744");
 const Charlie = "5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y"
-
 async function main () {
     const api = await ApiPromise.create({ 
         provider: wsProvider,
         types: { 
-            Address: 'AccountId', 
-            LookupSource: 'AccountId', 
-            RelayChainBlockNumber: 'BlockNumber',
-            ValidationFunction: 'Vec<u8>',
-            ValidationFunctionParams: {
-              max_code_size: 'u32',
-              relay_chain_height: 'RelayChainBlockNumber',
-              code_upgrade_allowed: 'Option<RelayChainBlockNumber>'
+            "Address": "AccountId", 
+            "LookupSource": "AccountId", 
+            "RelayChainBlockNumber": "BlockNumber",
+            "ValidationFunction": "Vec<u8>",
+            "ValidationFunctionParams": {
+              "max_code_size": "u32",
+              "relay_chain_height": "RelayChainBlockNumber",
+              "code_upgrade_allowed": "Option<RelayChainBlockNumber>"
             }
         }
      });
 
-    const keyring = new Keyring({ type: 'sr25519' });
-    const alice = keyring.addFromUri('//Alice');
+    const keyring = new Keyring({ type: "sr25519" });
+    const alice = keyring.addFromUri("//Alice");
 
     // This is currently invalid
-    const unsub = await api.tx.tokenDealer.transferTokensToRelayChain(Charlie, 12345)
-    .signAndSend(alice, ({ events = [], status }) => {
-        console.log(`Current status is ${status.type}`);
-    
-        if (status.isFinalized) {
-          console.log(`Transaction included at blockHash ${status.asFinalized}`);
-    
-          // Loop through Vec<EventRecord> to display all events
-          events.forEach(({ phase, event: { data, method, section } }) => {
-            console.log(`\t' ${phase}: ${section}.${method}:: ${data}`);
-          });
-    
-          unsub();
-        }
-  })
+    const unsub = await api.tx.balances.transfer(Charlie, 100)
+    .signAndSend(alice, (result) => {
+        console.log(`Current result is ${JSON.stringify(result)}`);
+
+        // Current result is {"events":[],"status":{"Ready":null}}
+        // Current result is {"events":[],"status":{"Invalid":null}}
+    })
 };
   
   main()
